ac40686f25686598fb0453e7e3833491b601dfcd
Lukáš Havrlant <lukas@havrlant.cz>
2016-11-01 23:37:16 +0100

    gulp-watch changed

--- gulptasks/watch.js
+++ gulptasks/watch.js
@@ -1,17 +1,30 @@
-module.exports = function (gulp, config) {
-    const watch = require('gulp-chokidar')(gulp);
+const watch = require('gulp-watch');
 
+module.exports = function (gulp, config) {
     gulp.task('watch', function() {
-        watch(config.src.js.inputFiles, ['javascript']);
-        watch(config.src.html.inputFiles, ['html']);
-        watch(config.src.less.inputFiles, ['less']);
+
+        watch(config.src.js.inputFiles, function () {
+            gulp.start('javascript');
+        });
+
+        watch(config.src.html.inputFiles, function () {
+            gulp.start('html');
+        });
+
+        watch(config.src.less.inputFiles, function () {
+            gulp.start('less');
+        });
 
         if (config.src.assets) {
-            watch(config.src.assets.inputFiles, ['assets']);
+            watch(config.src.assets.inputFiles, function () {
+                gulp.start('assets');
+            });
         }
 
         if (config.src.images) {
-            watch(config.src.images.inputFiles, ['images']);
+            watch(config.src.images.inputFiles, function () {
+                gulp.start('images');
+            });
         }
     });
 };
