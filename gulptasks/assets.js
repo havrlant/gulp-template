dcfe5122faf9689a8bf1d83b4a11c58f2e0d1295
Lukáš Havrlant <lukas@havrlant.cz>
2016-11-01 23:20:34 +0100

    use cache

--- gulptasks/assets.js
+++ gulptasks/assets.js
@@ -1,10 +1,12 @@
 const connect = require('gulp-connect');
+const cache = require('gulp-cached');
 
 module.exports = function (gulp, config) {
     gulp.task('assets', function () {
         if (!config.src.assets) return;
 
         return gulp.src(config.src.assets.inputFiles)
+            .pipe(cache('assets', {optimizeMemory: true}))
             .pipe(gulp.dest(config.src.assets.outputDir))
             .pipe(connect.reload());
     });
--- gulptasks/html.js
+++ gulptasks/html.js
@@ -1,8 +1,10 @@
 const connect = require('gulp-connect');
+const cache = require('gulp-cached');
 
 module.exports = function (gulp, config) {
     gulp.task('html', function () {
         return gulp.src(config.src.html.inputFiles)
+            .pipe(cache('html', {optimizeMemory: true}))
             .pipe(gulp.dest(config.src.html.outputDir))
             .pipe(connect.reload());
     });
